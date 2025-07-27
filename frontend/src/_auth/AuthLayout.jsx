8bbb1e685abaa26287ecfa47d2f1e83526d69851
Ralph Carandang <rvncrndng@gmail.com>
2025-07-27 23:54:15 +0800

    Login and Register UI

--- frontend/src/_auth/AuthLayout.jsx
+++ frontend/src/_auth/AuthLayout.jsx
@@ -4,13 +4,17 @@ import Navbar from '../sections/Navbar'
 import Footer from '../sections/Footer'
 const AuthLayout = () => {
   return (
-    <div>
-      <Navbar/>
-      <div className='min-h-screen max-container padding-x'>
-        <Outlet/>
+   <div className="flex flex-col min-h-screen">
+      <Navbar />
+  
+      <div className="flex-1 mb-10">
+        <div className="max-container padding-x">
+          <Outlet />
+        </div>
       </div>
-         <Footer/>
-    </div>
+
+      <Footer />
+</div>
   )
 }
 

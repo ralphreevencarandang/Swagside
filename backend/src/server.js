03b9d4b1cd667663ca0f5d080b4906fa92f540d1
Ralph Carandang <rvncrndng@gmail.com>
2025-08-13 16:34:02 +0800

    config: origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5001;
 app.use(cookieParser())
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://swagside.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
